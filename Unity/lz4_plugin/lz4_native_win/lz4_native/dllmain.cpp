c65b9568651e17cd60e3d0c44b4f6c1d7451747d
umetaman <umeta@w0w.co.jp>
2022-03-25 10:41:25 +0900

    Add Lz4 Plugin API for windows

--- Unity/lz4_plugin/lz4_native_win/lz4_native/dllmain.cpp
+++ Unity/lz4_plugin/lz4_native_win/lz4_native/dllmain.cpp
@@ -1,6 +1,8 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "stdafx.h"
 #include "lz4.h"
+#include "lz4frame.h"
+#include "lz4hc.h"
 
 #define DLLAPI extern "C" __declspec(dllexport)
 
@@ -23,4 +25,16 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 
 DLLAPI int lz4_decompress_safe_native(const char* source, char* dest, int compressedSize, int maxDecompressedSize) {
 	return LZ4_decompress_safe(source, dest, compressedSize, maxDecompressedSize);
+}
+
+DLLAPI int lz4_compress_default_native(const char* source, char* dest, int srcSize, int dstCapacity) {
+	return LZ4_compress_default(source, dest, srcSize, dstCapacity);
+}
+
+DLLAPI int lz4_compressBound_native(int inputSize) {
+	return LZ4_compressBound(inputSize);
+}
+
+DLLAPI int lz4_compress_HC_native(const char* source, char* dest, int srcSize, int maxDstSize, int compressionLevel) {
+	return LZ4_compress_HC(source, dest, srcSize, maxDstSize, compressionLevel);
 }
