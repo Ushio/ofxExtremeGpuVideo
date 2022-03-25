95d28b5ab2de0e009316467a46d2263dca6b7cc9
umetaman <umeta@w0w.co.jp>
2022-03-25 17:54:05 +0900

    Add GpuVideoEncoder for Unity

--- Unity/Example/Assets/GpuVideo/GpuVideoPlayer.cs
+++ Unity/Example/Assets/GpuVideo/GpuVideoPlayer.cs
@@ -4,94 +4,104 @@
 using UnityEditor;
 #endif
 
-public class GpuVideoPlayer : MonoBehaviour {
-    public string PathForStreamingAssets = "";
-    public Renderer RendererCompornent;
-    public float TimeAt = 0.0f;
-    public bool IsPause = false;
-    public bool IsLoop = false;
-
-	GpuVideo _video;
-	Material _material;
-
-	// Use this for initialization
-	void Start () {
-        
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        if(_material == null)
-        {
-            _material = RendererCompornent.material;
-            _material.mainTexture = _video.Texture;
-        }
-        if(IsPause == false)
-        {
-            TimeAt += Time.deltaTime;
-
-            if(IsLoop && _video.Duration < TimeAt)
-            {
-                TimeAt = 0.0f;
-            }
-        }
-
-        _video.setTime(TimeAt);
-	}
-
-	void OnEnable() {
-		_video = new GpuVideo (PathForStreamingAssets);
-    }
-	void OnDisable() {
-		_material.mainTexture = null;
-		_video.Dispose ();
-		_video = null;
-	}
-}
-
-#if UNITY_EDITOR
-[CustomEditor(typeof(GpuVideoPlayer))]
-public class GpuVideoPlayerEditor : Editor
+namespace ExtremeGpuVideo
 {
-    float _position = 0.0f;
-    GpuVideo _video;
-
-
-    void OnEnable()
+    public class GpuVideoPlayer : MonoBehaviour
     {
-    }
-    void OnDisable()
-    {
-        if(_video != null)
+        public string PathForStreamingAssets = "";
+        public Renderer RendererCompornent;
+        public float TimeAt = 0.0f;
+        public bool IsPause = false;
+        public bool IsLoop = false;
+
+        GpuVideo _video;
+        Material _material;
+
+        // Use this for initialization
+        void Start()
         {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (_material == null)
+            {
+                _material = RendererCompornent.material;
+                _material.mainTexture = _video.Texture;
+            }
+            if (IsPause == false)
+            {
+                TimeAt += Time.deltaTime;
+
+                if (IsLoop && _video.Duration < TimeAt)
+                {
+                    TimeAt = 0.0f;
+                }
+            }
+
+            _video.setTime(TimeAt);
+        }
+
+        void OnEnable()
+        {
+            _video = new GpuVideo(PathForStreamingAssets);
+        }
+        void OnDisable()
+        {
+            _material.mainTexture = null;
             _video.Dispose();
             _video = null;
         }
     }
 
-    public override void OnInspectorGUI()
+#if UNITY_EDITOR
+    [CustomEditor(typeof(GpuVideoPlayer))]
+    public class GpuVideoPlayerEditor : Editor
     {
-        DrawDefaultInspector();
+        float _position = 0.0f;
+        GpuVideo _video;
 
-        try
+
+        void OnEnable()
         {
-            GpuVideoPlayer myScript = (GpuVideoPlayer)target;
-
-            if (_video == null || _video.PathForStreamingAssets != myScript.PathForStreamingAssets)
+        }
+        void OnDisable()
+        {
+            if (_video != null)
             {
-                if(_video != null)
-                {
-                    _video.Dispose();
-                }
-                _video = new GpuVideo(myScript.PathForStreamingAssets);
+                _video.Dispose();
+                _video = null;
             }
+        }
 
-            _video.setTime(_video.Duration * _position);
-            GUILayout.Label(_video.Texture, GUILayout.Width(300), GUILayout.Height(200));
-            _position = GUILayout.HorizontalSlider(_position, 0.0f, 1.0f);
-        } catch (System.Exception ) {
-            // :(
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            try
+            {
+                GpuVideoPlayer myScript = (GpuVideoPlayer)target;
+
+                if (_video == null || _video.PathForStreamingAssets != myScript.PathForStreamingAssets)
+                {
+                    if (_video != null)
+                    {
+                        _video.Dispose();
+                    }
+                    _video = new GpuVideo(myScript.PathForStreamingAssets);
+                }
+
+                _video.setTime(_video.Duration * _position);
+                GUILayout.Label(_video.Texture, GUILayout.Width(300), GUILayout.Height(200));
+                _position = GUILayout.HorizontalSlider(_position, 0.0f, 1.0f);
+            }
+            catch (System.Exception)
+            {
+                // :(
+            }
         }
     }
-}
 #endif
+}
