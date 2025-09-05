cd16306b60ee0cde994f6a59acb061c3142d43ce
umeta <umeta@w0w.co.jp>
2025-09-05 10:32:02 +0900

    Add iOS Plugin.

---
+++ Unity/Example/Assets/Plugins/ios/main.h
@@ -0,0 +1,14 @@
+//
+//  main.h
+//  lz4_native_osx
+//
+//  Created by Nakazi_w0w on 4/1/16.
+//  Copyright © 2016 wow. All rights reserved.
+//
+
+#ifndef main_h
+#define main_h
+
+#include <stdio.h>
+
+#endif /* main_h */
