2300a9065fae67e816d5dcc45ae25d9f22070b96
umeta <umeta@w0w.co.jp>
2025-09-05 10:32:49 +0900

    Fix DllImport

--- Unity/Example/Assets/GpuVideo/Lz4Native.cs
+++ Unity/Example/Assets/GpuVideo/Lz4Native.cs
@@ -10,16 +10,22 @@ namespace ExtremeGpuVideo.Plugin
 	{
 		public const int LZ4HC_CLEVEL_DEFAULT = 9;
 
-		[DllImport("lz4_native")]
+#if UNITY_IOS && !UNITY_EDITOR
+		const string dll = "__Internal";
+#else
+        const string dll = "lz4_native";
+#endif
+
+        [DllImport(dll)]
 		public static extern int lz4_decompress_safe_native(IntPtr source, IntPtr dest, int compressedSize, int maxDecompressedSize);
 
-		[DllImport("lz4_native")]
+		[DllImport(dll)]
 		public static extern int lz4_compress_default_native(IntPtr source, IntPtr dest, int srcSize, int maxDestSize);
 
-		[DllImport("lz4_native")]
+		[DllImport(dll)]
 		public static extern int lz4_compressBound_native(int inputSize);
 
-		[DllImport("lz4_native")]
+		[DllImport(dll)]
 		public static extern int lz4_compress_HC_native(IntPtr source, IntPtr dest, int srcSize, int maxDstSize, int compressionLevel);
 	}
 }
